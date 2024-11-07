baaa9dd4b00761614300e6aa53cf9af66360955d
Evan Harden <evan145447@gmail.com>
2024-11-06 21:42:42 -0500

    Stylized sign up, Appointment creation, and Appointment Index. Added Appointment show page.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,23 +2,24 @@ class AppointmentsController < ApplicationController
     def index
         @appointments = Appointment.all
     end
-
-
     def new
         @appointment = Appointment.new
     end
-
     def create
         @appointment = Appointment.new(appointment_params)
+        @appointment.customer_id = current_user.id
         if @appointment.save
           redirect_to appointments_path, notice: "Appointment was successfully created."
         else
           render :new, alert: "There was an error creating the appointment."
         end
     end
-    
+    def show
+        @appointment = Appointment.find(params[:id])
+    end
+
     def appointment_params
-        params.require(:appointment).permit(:type, :status, :customer_id, :new_customer, :created_by, :completed_by, :scheduled_date, :scheduled_start, :scheduled_end, :note)
+        params.require(:appointment).permit(:appointment_type, :status, :customer_id, :new_customer, :created_by, :completed_by, :scheduled_date, :scheduled_start, :scheduled_end, :note)
     end
 end
   
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,9 @@
 class Appointment < ApplicationRecord
+    validates :scheduled_date, presence: true
+    validates :scheduled_start, presence: true
+    validates :scheduled_end, presence: true
+    validates :appointment_type, presence: true
+    validates :status, presence: true
+    validates :customer_id, presence: true
+    validates :note, presence: true
 end
