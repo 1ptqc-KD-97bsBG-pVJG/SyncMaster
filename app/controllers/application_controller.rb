3dd8d6a4843d03ef8d37810d3f4103ea808902bc
1ptqc-KD-97bsBG-pVJG <76831037+1ptqc-KD-97bsBG-pVJG@users.noreply.github.com>
2024-11-12 21:38:30 -0500

    added user managment page, edit users, font awesome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # Allow additional fields during sign up
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    # Allow additional fields during account update
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update(user_params)
+          format.html { redirect_to '/users/index', notice: "User was successfully updated." }
+          format.json { render :show, status: :ok, location: @user }
+      else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    puts "Inside destroy!"
+    respond_to do |format|
+      if @user.destroy
+        format.html { redirect_to '/users/index', notice: "User was successfully deleted." }
+        format.json { head :no_content }
+      else
+        format.html { redirect_to root_path, alert: "User could not be deleted, please contact a developer for assistance." }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :role)
+  end
+
+
+end
