d943b5c8d1f39184e1f24d9cb697fe320051c903
1ptqc-KD-97bsBG-pVJG <76831037+1ptqc-KD-97bsBG-pVJG@users.noreply.github.com>
2024-11-13 23:47:22 -0500

    added projects index pages

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,30 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+    # all projects with status less than 7
+    @active_projects = Project.where("status < 7")
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    @appointments = @project.appointments
+  end
+
+  def completed
+    @completed_projects = Project.where("status = 7 OR status = 8")
+    @current_monday = Time.zone.now.beginning_of_week
+    @current_month = Time.zone.now.beginning_of_month
+  end
+
+  def canceled
+    @canceled_projects = Project.where("status = 9")
+    @current_monday = Time.zone.now.beginning_of_week
+    @current_month = Time.zone.now.beginning_of_month
+  end
+
+  def deleted
+    @deleted_projects = Project.where("status = 10")
+    @current_monday = Time.zone.now.beginning_of_week
+    @current_month = Time.zone.now.beginning_of_month
+  end
+end
